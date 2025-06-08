bf08663a6d7cedacf15fc1cc8dab4256033178cf
Zubair Ibne Zaman <zubairzamanprof@gmail.com>
2025-06-08 17:48:06 +0600

    Delete SQLQuery2.sql

--- SQLQuery2.sql
+++
@@ -1,7 +0,0 @@
-SELECT TOP (1000) [continent]
-      ,[location]
-      ,[date]
-      ,[population]
-      ,[new_vaccinations]
-      ,[RollingCountofVacciation]
-  FROM [ProjectPortfolio].[dbo].[PopvsVac]
