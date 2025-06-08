40853c7e51304a173370b48a40fdca9021e6de03
Zubair Ibne Zaman <zubairzamanprof@gmail.com>
2025-06-08 04:45:00 -0700

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,7 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[RollingCountofVacciation]
+  FROM [ProjectPortfolio].[dbo].[PopvsVac]
